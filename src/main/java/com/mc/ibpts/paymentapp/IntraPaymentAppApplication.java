30e05dd0631b3d56fc07d43fe34b6c043250bf68
Arjun R <arjunr1432@gmail.com>
2021-08-04 22:49:22 +0200

    Initial commit with repo structure created from spring initializer.

---
+++ src/main/java/com/mc/ibpts/paymentapp/IntraPaymentAppApplication.java
@@ -0,0 +1,13 @@
+package com.mc.ibpts.paymentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntraPaymentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntraPaymentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mc/ibpts/paymentapp/IntraPaymentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc.ibpts.paymentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntraPaymentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
